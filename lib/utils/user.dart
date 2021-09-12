bbd01fcaed780a707c49b5c47e39f7b77b3d3b65
Ivan111582 <ivan111582@hotmail.com>
2021-09-11 21:25:06 -0500

    crear organización desde secure storage

--- lib/utils/user.dart
+++ lib/utils/user.dart
@@ -1,15 +1,22 @@
 
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 import 'utility.dart';
 
 class User {
-
+  final storage = new FlutterSecureStorage();
   static String userName='';
   static String direcc='';
   static String tele='';
   static Future<Null> setInfoFromLocalStorage() async {
-    userName  = await Utility.getLocalStorage('userName');
+    final storage = new FlutterSecureStorage();
+    /*userName  = await Utility.getLocalStorage('userName');
     direcc    = await Utility.getLocalStorage('direcc');
     tele      = await Utility.getLocalStorage('tele');
+    */
+    userName  =await storage.read(key: 'name') ?? '';
+    direcc    =await storage.read(key: 'direcc') ?? '';
+    tele      =await storage.read(key: 'tele') ?? '';
   }
   static Future<Null> login(userName, direcc, tele ) async {
     await Utility.setLocalStorage('userName', userName);
